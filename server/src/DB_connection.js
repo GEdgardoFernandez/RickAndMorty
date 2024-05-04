bbc79143fddd58c500c69d19b772712a1243942a
GuilleCOzzi <gecozzi87@gmail.com>
2024-05-04 00:01:01 -0300

    implementada la homework ORM 1 del modulo 04

---
+++ server/src/DB_connection.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const User = require('./models/User');
+const Favorite = require('./models/Favorite');
+
+// EJERCICIO 03
+// A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
+// Recuerda pasarle la información de tu archivo '.env'.
+
+
+// URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
+const sequelize = new Sequelize(
+   // URL
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   { logging: false, native: false }
+);
+
+// EJERCICIO 05
+// Debajo de este comentario puedes ejecutar la función de los modelos.
+
+//
+
+User(sequelize);
+Favorite(sequelize);
+//
+
+// Ejercicio 06
+// ¡Relaciona tus modelos aquí abajo!
+// const { User, Favorite } = sequelize.models;
+const { User, Favorite } = sequelize.models;
+
+User.hasMany(Favorite);
+Favorite.belongsTo(User);
+
+module.exports = {
+   User,
+   Favorite,
+   conn: sequelize,
+};
