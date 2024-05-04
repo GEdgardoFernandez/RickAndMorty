5926f9ce9a0c8ad3a905470cf75a76c6a83be903
GuilleCOzzi <gecozzi87@gmail.com>
2024-05-04 00:02:05 -0300

    implementada la homework m4

--- server/src/DB_connection.js
+++ server/src/DB_connection.js
@@ -28,6 +28,11 @@ Favorite(sequelize);
 // Ejercicio 06
 // ¡Relaciona tus modelos aquí abajo!
 // const { User, Favorite } = sequelize.models;
+// Relaciona los modelos en una relación de muchos a muchos
+User.belongsToMany(Favorite, { through: 'user_favorite' });
+Favorite.belongsToMany(User, { through: 'user_favorite' });
+
+
 const { User, Favorite } = sequelize.models;
 
 User.hasMany(Favorite);
